102d7068b3e0730f71ad06f9f0e960e30235ff66
F1reFinger <180121847@aluno.unb.br>
2022-08-30 15:29:47 -0300

    Added_Sources to test and implemented colisions and sprites

---
+++ Player.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export var speed = 100
+var screen_size
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("Move_right"):
+		velocity.x += 100
+	if Input.is_action_pressed("Move_left"):
+		velocity.x -= 100
+	
+	if Input.is_action_pressed("Jump"):
+		velocity.y -= 100
+	if Input.is_action_pressed("Down"):
+		velocity.y += 100
+	
+	position += velocity * delta
+	print("X: ", position.x, " Y: ", position.y)
+	print($Sprite.position)
+	
