c411fd48442f295d1a30f2a2f2ae3e54914179da
Gabriel-Azevedo-Batalha <batalha800@gmail.com>
2022-09-01 17:51:49 -0300

    Added boss attacks

---
+++ Projectile.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+signal kill
+
+var gravity = 10000
+export (float) var speed = 1
+var y = position.y
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	rng.randomize()
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()
+	velocity.y += (gravity * speed) * delta
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+	for i in get_slide_count():
+		var collision_info = get_slide_collision(i)
+		if collision_info.collider is TileMap:
+			position.y = y
+			position.x = rng.randf_range(18, 1032)
+			$Sound.play()
+		elif collision_info.collider.name == "Player":
+			collision_info.collider.die()
+		elif "Projectile" in collision_info.collider.name:
+			self.add_collision_exception_with(collision_info.collider)
